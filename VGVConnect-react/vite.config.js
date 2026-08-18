caa28d8e21659b936a28383c8693d534246a66e0
fepo22 <olea.mau@gmail.com>
2026-08-18 16:40:16 -0400

    fix: allow Railway preview hosts

--- VGVConnect-react/vite.config.js
+++ VGVConnect-react/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['front-production-a04e.up.railway.app'],
+    allowedHosts: true,
   },
 })
